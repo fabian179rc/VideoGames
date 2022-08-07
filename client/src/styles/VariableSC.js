786a1cebe899a3d9c7b5d6a9d1b3c66ce2dc9a4a
unknown <fabian179rc@gmail.com>
2022-08-07 15:39:06 -0300

    4

---
+++ client/src/styles/VariableSC.js
@@ -0,0 +1,11 @@
+export const RED = '#dd163b';
+// #dd163b
+// #ef4565
+export const DARK = '#181c23';
+export const DARKL = '#181c2399';
+export const BLACK ='rgba(0,0,0,.9)';
+export const WHITEL ='#fcfcfc17';
+export const WHITE ='#fcfcfc';
+export const BLUE ='#2943d1';
+
+export const plataformsArray= ['Atari ST','Linux','Game Boy Advance','GameCube','macOS','Nintendo Switch','Nintendo 3DS', 'PC','PlayStation 1','PlayStation 2','PlayStation 3','PlayStation 4', 'PlayStation 5','PS Vita', 'Xbox 360','Xbox One','Xbox Series S/X','SEGA','Wii  ','Wii U']
